81cba66b400aa7a8ecbaac4e4057ab514d5d9e00
Frank E Weiss <feweiss@gmail.com>
2017-01-14 11:02:12 -0800

    Remove dead code

--- wireless-switch.ino
+++ wireless-switch.ino
@@ -14,13 +14,9 @@
 
 #include "web-template.h"
 
-WebTemplate webTemplate;
-
 const char WiFiAPPSK[] = "sparkfun";
 
 const int LED_PIN = 4; // Thing's onboard, green LED
-const int ANALOG_PIN = A0; // The only analog pin on the Thing
-const int DIGITAL_PIN = 12; // Digital pin to be read
 
 boolean state = false;
 
@@ -101,7 +97,6 @@ void setup() {
             server.send(400, "text/plain", "missing data 'state'");
         }
         state = strcmp("on", root["state"]) == 0;
-        //digitalWrite(LED_PIN, state);
         switchOn(state);
         server.send(200);
     });
@@ -111,97 +106,6 @@ void setup() {
 void loop() {
     server.handleClient();
 }
-
-/*
-void xloop()
-{
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-
-  // Match the request
-  int val = -1; // We'll use 'val' to keep track of both the
-                // request type (read/set) and value if set.
-
-    if (req.indexOf("/main.css") != -1) {
-        client.print(webTemplate.getCss());
-        return;
-    }
-  if (req.indexOf("/led/0") != -1)
-    val = 0; // Will write LED low
-  else if (req.indexOf("/led/1") != -1)
-    val = 1; // Will write LED high
-  else if (req.indexOf("/read") != -1)
-    val = -2; // Will print pin reads
-  // Otherwise request will be invalid. We'll say as much in HTML
-
-  // Set GPIO5 according to the request
-  if (val >= 0)
-    digitalWrite(LED_PIN, val);
-
-  client.flush();
-
-  // Prepare the response. Start with the common header:
-  String style =
-    "body { font-size: 200%; }"
-    "div { width: auto; padding: 1em 0; }"
-    "a { display: block; }"
-    "button { width: 100%; height: 10%; margin: 1em 0; font-size: 100%; font-weight: bold; }";
-
-  String s = "HTTP/1.1 200 OK\r\n";
-  s += "Content-Type: text/html\r\n\r\n";
-  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
-  s += "<head>";
-  s += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-//  s += "<style>" + style + "</style>";
-    s += "<link rel=\"stylesheet\" href=\"/main.css\"/>";
-  s += "</head>";
-  s += "<body>";
-  // If we're setting the LED, print out a message saying we did
-  if (val >= 0)
-  {
-    s += "LED is now ";
-    s += (val)?"on":"off";
-  }
-  else if (val == -2)
-  { // If we're reading pins, print out those values:
-    s += "Analog Pin = ";
-    s += String(analogRead(ANALOG_PIN));
-    s += "<br>"; // Go to the next line.
-    s += "Digital Pin 12 = ";
-    s += String(digitalRead(DIGITAL_PIN));
-  }
-  else
-  {
-    s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
-  }
-  s +=       " <label class=\"switch\">";
-  s +=                "<input type=\"checkbox\"/>";
-  s +=                 "<div class=\"slider\"></div>";
-  s +=             "</label>";
-
- // s += "<div>";
- // s += "<a href=\"/led/1\"><button>Off</button></a>";
- // s += "<a href=\"/led/0\"><button>On</button></a>";
- // s += "</body></html>\n";
- // s += "</div>";
-
-  // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disonnected");
-
-  // The client will actually be disconnected
-  // when the function returns and 'client' object is detroyed
-}
-*/
 
 void setupWifiAccessPoint()
 {
@@ -230,14 +134,6 @@ void setupWifiStation() {
   WiFi.begin(LOCAL_WIFI_STA_SSID, LOCAL_WIFI_STA_PASS);
 }
 
-
-void initHardware()
-{
-  Serial.begin(115200);
-  pinMode(DIGITAL_PIN, INPUT_PULLUP);
+void initHardware() {
   pinMode(LED_PIN, OUTPUT_OPEN_DRAIN); // OUTPUT, OUTPUT_OPEN_DRAIN
-  //digitalWrite(LED_PIN, LOW);
-  //state = HIGH;
-  // Don't need to set ANALOG_PIN as input,
-  // that's all it can be.
 }
